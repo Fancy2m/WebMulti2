808281d22dcfce8692637661c6278ebd343154af
Marcel <marcel.rosik@stud.hs-ruhrwest.de>
2018-06-30 16:46:53 +0200

    sdfghjklö

--- loginpage.php
+++ loginpage.php
@@ -2,11 +2,11 @@
 session_start();
 include "index.php";
 include "login.php";
-if ($_SESSION['gruppe']==2) {
-  include "adminmenu.php";
-}
-elseif ($_SESSION['gruppe']==1)
-  {
-    include "usermenu.php";
-  }
+//if ($_SESSION['gruppe']==2) {
+  //include "adminmenu.php";
+//}
+//elseif ($_SESSION['gruppe']==1)
+  //{
+    //include "usermenu.php";
+  //}
  ?>
