223f67ccd34e49e2248e78a1244d20639b1f3f49
Marcel <marcel.rosik@stud.hs-ruhrwest.de>
2018-06-30 16:57:38 +0200

    ASDFGHJK

--- usermenu.php
+++ usermenu.php
@@ -2,6 +2,7 @@
 session_start();
 if ($_SESSION['login']==1) {
   echo '<div class=usermenu>
+    <form action="loginpage.php" method="POST">
     <!--<form action="usermenu.php" method="GET"> -->
       <button onClick="window.location.href="user.php?var=1"">Profil</button>
     <!--</form> -->
@@ -10,7 +11,7 @@ if ($_SESSION['login']==1) {
     <!-- </form> -->
       <button onClick="window.location.href="user.php?var=3"">Meine Bewertungen</button>
       <!--<button onClick="window.location.href="user.php?var=4""></button>-->
-      <form action="loginpage.php" method="POST">
+
         <button type="submit" name="lgbutton" value="Logout">Ausloggen</button>
       </form>
   </div>';
