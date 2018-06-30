2ff062f5447e5d3c382511168fc4bfeedefcfcb2
JulicR <40716299+JulicR@users.noreply.github.com>
2018-06-30 15:57:51 +0200

    ich habe gemacht

--- searchtest1.php
+++ searchtest1.php
@@ -1,5 +1,53 @@
+<?php
+	session_start();
+	if($_POST['suchen'] == "Suchen")
+	{
+		if(!$link=mysqli_connect("127.0.0.1","root",""))
+		{
+			echo "Verbindungsaufbau gescheitert.";
+		}
+		else
+		{
+			mysqli_set_charset($link,"utf8");
+			$db=mysqli_select_db($link, "webmult");
+      $suche=$POST['search'];
+			$abfrage="select * from film join director on director_id=director_fid where film.name like %'$suche'% or director.dnachname like %'$suche'%;
+			$ergebnis=mysqli_query($link,$abfrage);
+      echo "<table border='1'>
+      <tr>
+      <th>Filmtitel</th>
+      <th>Erscheinungsjahr</th>
+      <th>Directorvorname</th>
+      <th>Director Nachname</th>
+      <th>Beschreibung</th>
+      <th>Durchschnittsrating</th>
+      <th>Altersfreigabe</th>
+      </tr>";
+
+      while($row = mysqli_fetch_array($result))
+      {
+        echo "<tr>";
+        echo '<td><textarea name="filmname">'; echo $row['name']; echo '</textarea></td>';
+        echo "<td><input type='textarea' value=" . $row['erscheinungsjahr'] . "></input></td>";
+        echo "<td><input type='textarea' value=" . $row['dvorname'] . "></input></td>";
+        echo "<td><input type='textarea' value=" . $row['dnachname'] . "></input></td>";
+        echo "<td><input type='textarea' value=" . $row['beschreibung']. "></input></td>";
+        echo "<td><input type='textarea' value=" . $row['avgrating'] . "></input></td>";
+        echo "<td><input type='textarea' value=" . $row['fsk'] . "></input></td>";
+        echo '<td> <button name="edit"  value="edit" type="submit">Speichern</button> </td>';
+        echo "</tr>";
+      }
+      echo "</table>";
+		}
+  }
+?>
+
 <html>
   <body>
+    <form action="searchtest1.php" method="POST">
     <input type="text" name="search">
+    <input type="submit" name="suchen" value="Suchen">
+
+    </form>
   </body>
 </html>
