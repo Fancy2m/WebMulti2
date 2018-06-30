1952eb6504aadf436924667db3d3d266b2e1f758
JulicR <40716299+JulicR@users.noreply.github.com>
2018-06-30 16:50:56 +0200

    Merge branch 'master' of https://github.com/Fancy2m/WebMulti2

--- loginpage.php
+++ loginpage.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
 include "index.php";
 include "login.php";
+//if ($_SESSION['gruppe']==2) {
+  //include "adminmenu.php";
+//}
+//elseif ($_SESSION['gruppe']==1)
+  //{
+    //include "usermenu.php";
+  //}
  ?>
