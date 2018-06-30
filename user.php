2bee7fe3ded66c4bc39837272df1c5a5a03d156f
Fancy2m <39697037+Fancy2m@users.noreply.github.com>
2018-07-01 00:33:11 +0200

    a

--- user.php
+++ user.php
@@ -18,8 +18,7 @@ session_start();
         include "usermenu.php";
         if($wert==1)
           {// Meine Daten und Statistiken
-
-            $abfrage="SELECT COUNT(*) FROM rating where user_rid='".$_SESSION['userID']."' LIMIT 1";
+            $abfrage="SELECT COUNT(*) FROM rating where user_rid='.$_SESSION['userID'].' LIMIT 1";
       			$ergebnis=mysqli_query($link,$abfrage);
       			$row=mysqli_fetch_row ($ergebnis);
             echo "Willkommen,"; echo $_SESSION['nutzervorname'] ."</br>";
