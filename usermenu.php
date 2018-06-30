08559568021d437b3419031084f7470b1d8432f3
Marcel <marcel.rosik@stud.hs-ruhrwest.de>
2018-06-30 18:45:50 +0200

    usermenu...

--- usermenu.php
+++ usermenu.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 if ($_SESSION['login']==1) {
-  echo '    <button onClick="window.location.href="user.php?var=1"">Nutzerverwaltung</button>
-';
+  echo "Ja";
 }
 else {
   echo "NEIN!!!";
@@ -14,8 +13,9 @@ else {
   </head>
   <body>
     <div class=usermenu>
-      <button onClick='window.location.href='user.php?var=2''>Mein Konto</button>
-      <button onClick='window.location.href='user.php?var=3''>Meine Bewertungen</button>
+    <button onClick="window.location.href='user.php?var=1'">Nutzerverwaltung</button>
+      <button onClick='window.location.href="user.php?var=2"'>Mein Konto</button>
+      <button onClick='window.location.href="user.php?var=3"'>Meine Bewertungen</button>
       <form action='loginpage.php' method='POST'>
         <button type='submit' name='lgbutton' value='Logout'>Ausloggen</button>
       </form>
