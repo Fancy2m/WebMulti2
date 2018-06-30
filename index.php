3835df97c26441414b1b7eed0b161fcfdedffa69
Daniel Keib <daniel-keib@hotmail.de>
2018-06-30 14:44:12 +0200

    j

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 </head>
 <body>
   <h1>Film DB</h1>
-  <a href="/webmulti2/login.php">Login</a>
-  <a href="/webmulti2/register.php">Registrieren</a>
+  <a class="login" href="/webmulti2/login.php">Login</a>
+  <a class="login" href="/webmulti2/register.php">Registrieren</a>
 </body>
