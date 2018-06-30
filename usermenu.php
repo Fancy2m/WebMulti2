b236ec71adc57287f0b28c7d19e413de11c61239
Marcel <marcel.rosik@stud.hs-ruhrwest.de>
2018-06-30 18:41:40 +0200

    usermenu kacke

--- usermenu.php
+++ usermenu.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
 if ($_SESSION['login']==1) {
-  echo '<div class=usermenu>
-    <button onClick="window.location.href="user.php?var=1"">Profil</button>
-    <button onClick="window.location.href="user.php?var=2"">Mein Konto</button>
-    <button onClick="window.location.href="user.php?var=3"">Meine Bewertungen</button>
-    <form action="loginpage.php" method="POST">
-      <button type="submit" name="lgbutton" value="Logout">Ausloggen</button>
-    </form>
-    </div>';
+  echo '    <button onClick="window.location.href="user.php?var=1"">Nutzerverwaltung</button>
+';
 }
 else {
   echo "NEIN!!!";
@@ -19,5 +13,12 @@ else {
   <head>
   </head>
   <body>
+    <div class=usermenu>
+      <button onClick='window.location.href='user.php?var=2''>Mein Konto</button>
+      <button onClick='window.location.href='user.php?var=3''>Meine Bewertungen</button>
+      <form action='loginpage.php' method='POST'>
+        <button type='submit' name='lgbutton' value='Logout'>Ausloggen</button>
+      </form>
+      </div>
   </body>
 </html>
